1ee0ec55f99ead0c491476697e2b9ff00d0ba757
Demo User <debian@beaglebone.localdomain>
2018-07-13 21:11:26 +0000

    change up some files

---
+++ sphinxbase/src/libsphinxbase/lm/jsgf_parser.h
@@ -0,0 +1,93 @@
+/* A Bison parser, made by GNU Bison 3.0.4.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_JSGF_PARSER_H_INCLUDED
+# define YY_YY_JSGF_PARSER_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    HEADER = 258,
+    GRAMMAR = 259,
+    IMPORT = 260,
+    PUBLIC = 261,
+    TOKEN = 262,
+    RULENAME = 263,
+    TAG = 264,
+    WEIGHT = 265
+  };
+#endif
+/* Tokens.  */
+#define HEADER 258
+#define GRAMMAR 259
+#define IMPORT 260
+#define PUBLIC 261
+#define TOKEN 262
+#define RULENAME 263
+#define TAG 264
+#define WEIGHT 265
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+
+union YYSTYPE
+{
+#line 65 "jsgf_parser.y" /* yacc.c:1909  */
+
+       char *name;
+       float weight;
+       jsgf_rule_t *rule;
+       jsgf_rhs_t *rhs;
+       jsgf_atom_t *atom;
+
+#line 82 "jsgf_parser.h" /* yacc.c:1909  */
+};
+
+typedef union YYSTYPE YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+
+int yyparse (void* yyscanner, jsgf_t *jsgf);
+
+#endif /* !YY_YY_JSGF_PARSER_H_INCLUDED  */
